9e8114ce40fee19cd4ab9bbc6b8d02402b15f7a4
Maciej Walecki <maciejwalecki99@gmail.com>
2023-03-30 13:21:45 +0200

    build: initial commit

---
+++ backend/src/main/java/com/todo/todo/entities/Todo.java
@@ -0,0 +1,25 @@
+package com.todo.todo.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+
+}
